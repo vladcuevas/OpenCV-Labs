3306bb01926216069c47d11f26fac65dd0b168a3
vlad0x <vladcuevas@gmail.com>
2020-06-29 21:08:31 -0500

    channels and colors

--- Image/image.cpp
+++ Image/image.cpp
@@ -13,6 +13,21 @@
 using namespace cv;
 using namespace std;
 
+vector<int> setCompressionParameters(int jpg_quality)
+{
+  vector<int> compression_params;
+  compression_params.push_back(IMWRITE_JPEG_QUALITY);
+  compression_params.push_back(jpg_quality);
+  // compression_params.push_back(IMWRITE_JPEG_PROGRESSIVE);
+  // compression_params.push_back(1);
+  // compression_params.push_back(IMWRITE_JPEG_OPTIMIZE);
+  // compression_params.push_back(1);
+  // compression_params.push_back(IMWRITE_JPEG_LUMA_QUALITY);
+  // compression_params.push_back(30);
+
+  return compression_params;
+}
+
 std::string GetCurrentWorkingDir(void)
 {
   char buff[FILENAME_MAX];
@@ -65,9 +80,9 @@ string type2str(int type)
 
 int main()
 {
-  string DATA_DIR = GetCurrentWorkingDir();
+  string DATA_PATH = GetCurrentWorkingDir();
 
-  string inputFileName = DATA_DIR + "\\number_zero.jpg";
+  string inputFileName = DATA_PATH + "\\number_zero.jpg";
 
   std::cout << "Read image " << inputFileName << " in Grayscale format\n";
 
@@ -105,24 +120,115 @@ int main()
   cout << "\n\n";
 
   //set region of an image
-  testImage(Range(0, 2), Range(0, 4)).setTo(111);
+  testImage(Range(0, 2), Range(0, 4)).setTo(222);
   cout << "Modified Matrix\n"
        << testImage;
+
+  size_t lastindex = inputFileName.find_last_of(".");
+  string rawname = inputFileName.substr(0, lastindex);
+
+  //extension defines the type
+  imwrite(rawname + "_changed.jpg", testImage, setCompressionParameters(90));
 
   cout << "\n";
   cout << "\n";
 
   // Read an image
-  string imagePath = DATA_DIR + "\\boy.jpg";
+  string imagePath = DATA_PATH + "\\boy.jpg";
   cout << "Reading " + imagePath + "\n";
   Mat boy = imread(imagePath);
+  namedWindow("Boy", WINDOW_AUTOSIZE);
   // Display the image using imshow
   imshow("Boy", boy);
   // Wait for user to press a key
   waitKey(0);
   // Destroy the window
-  destroyAllWindows();
+
+  cout << "image size = " << boy.size() << endl;
+  cout << "image channels = " << boy.channels();
+  //3 channels mean that image is RGB
+  //4 channels mean that image is Alpha
+  //Blue channel is indexed first,
+  //followed by the Green Channel and finally the Red Channel
+
+  // Show the channels
+  Mat imgChannels[3];
+  split(boy, imgChannels);
+  cout << "\n";
+
+  cout << "blue channel\n";
+  imshow("Boy", imgChannels[0]);
+  waitKey(250);
+  cout << "green channel\n";
+  imshow("Boy", imgChannels[1]);
+  waitKey(250);
+  cout << "red channel\n";
+  imshow("Boy", imgChannels[2]);
+  waitKey(250);
+  imshow("Boy", boy);
+  waitKey(250);
+  destroyWindow("Boy");
+
+  testImage = imread(inputFileName, 1);
+
+  //Manipulating Color Pixels
+  namedWindow("Zero", WINDOW_NORMAL);
+  resizeWindow("Zero", 600, 600);
+  imshow("Zero", testImage);
+  waitKey(250);
 
   cout << "\n";
+  cout << testImage.at<Vec3b>(0, 0);
+
+  testImage.at<Vec3b>(0, 0) = Vec3b(0, 255, 255);
+  imshow("Zero", testImage);
+  waitKey(500);
+  testImage.at<Vec3b>(1, 1) = Vec3b(255, 255, 0);
+  imshow("Zero", testImage);
+  waitKey(500);
+  testImage.at<Vec3b>(2, 2) = Vec3b(255, 0, 255);
+  imshow("Zero", testImage);
+  waitKey(500);
+
+  testImage = imread(inputFileName, 1);
+  testImage(Range(0, 3), Range(0, 3)).setTo(Scalar(255, 0, 0));
+  imshow("Zero", testImage);
+  waitKey(500);
+  testImage(Range(3, 6), Range(0, 3)).setTo(Scalar(0, 255, 0));
+  imshow("Zero", testImage);
+  waitKey(500);
+  testImage(Range(6, 9), Range(0, 3)).setTo(Scalar(0, 0, 255));
+  imshow("Zero", testImage);
+  waitKey(500);
+  destroyWindow("Zero");
+
+  // Images with Alpha Channel
+  // In images with an alpha channel, each pixel not only has a color value, but also has a numerical transparency value ( between 0 to 255) that defines what will happen when the pixel is placed over another pixel.
+  // There are 4 channels, i.e. 3 color channels and 1 alpha channel which indicates the transparency.
+
+  imagePath = DATA_PATH + "/panther.png";
+
+  // Read the image
+  // Note that we are passing flag = -1 while reading the image ( it will read the image as is)
+  Mat imgPNG = imread(imagePath, -1);
+  cout << "image size = " << imgPNG.size() << endl;
+  cout << "number of channels = " << imgPNG.channels() << endl;
+
+  Mat imgBGR;
+  Mat imgPNGChannels[4];
+  split(imgPNG, imgPNGChannels);
+
+  merge(imgPNGChannels, 3, imgBGR);
+
+  Mat imgMask = imgPNGChannels[3];
+
+  namedWindow("Panther", WINDOW_AUTOSIZE);
+  imshow("Panther", imgBGR);
+  waitKey(500);
+  imshow("Panther", imgMask);
+  waitKey(500);
+
+  cout << "\n";
+  destroyAllWindows();
   return 0;
 }
