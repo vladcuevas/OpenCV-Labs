62ebe8fe65d66c2529c5437d3d43cdf6a418321d
vladcuevas <vladcuevas@gmail.com>
2022-10-25 13:02:07 -0500

    desaturation

--- 18_Image_Enhancement_and_Filtering/Color_Spaces/desaturationFilter.cpp
+++ 18_Image_Enhancement_and_Filtering/Color_Spaces/desaturationFilter.cpp
@@ -15,81 +15,97 @@ using namespace cv;
 
 namespace fs = std::filesystem;
 
-std::vector<std::string> tokenize(string s, string del = ",")
-{
-    int start, end = -1*del.size();
-    std::vector<std::string> tokens;
-    do {
-        start = end + del.size();
-        end = s.find(del, start);
-        tokens.push_back(s.substr(start, end - start));
-    } while (end != -1);
-
-    return tokens;
-}
-
 int main(int argc, char *argv[])
 {
+    // This program can be executed as an example without parameters
+    // also, can be called with two parameters,
+    // the first parameter is the image path
+    // the second parameter is 0 (to run without showing the images, aka silent mode)
+    // or 1 (to run showing the images)
+    // by default it will show the images, because this is an example
+
     std::string img_path = DATA_PATH + "images/girl.jpg";
+
+    int isImShow = 1;
+    fs::path fs_path;
+    fs::path parentStem;
 
     if (argc > 1) {
         img_path = argv[1];
+        fs_path = fs::path{img_path};
+        parentStem = fs_path.parent_path()/fs_path.stem();
+
+        try
+        {
+            isImShow = stoul(argv[2],nullptr,0);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+            isImShow = 0;
+        }
     }
 
-    // Read image
-    Mat img = imread(img_path);
+    if (fs::exists(fs_path)) {
+        // Read image
+        Mat img = imread(img_path);
 
-    imshow("Original Image", img);
-    waitKey(0);
+        if (isImShow == 1) {
+            imshow("Original Image", img);
+            waitKey(0);
+        }
 
-    // Specify scaling factor
-    float saturationScale = 0.01;
+        // Specify scaling factor
+        float saturationScale = 0.01f;
 
-    // Convert to HSV color space
-    Mat hsvImage;
-    cv::cvtColor(img, hsvImage, COLOR_BGR2HSV);
+        // Convert to HSV color space
+        Mat hsvImage;
+        cv::cvtColor(img, hsvImage, COLOR_BGR2HSV);
 
-    // Convert to float32
-    hsvImage.convertTo(hsvImage, CV_32F);
+        // Convert to float32
+        hsvImage.convertTo(hsvImage, CV_32F);
 
-    vector<Mat> channels(3);
+        vector<Mat> channels(3);
 
-    // Split the channels
-    split(hsvImage, channels);
+        // Split the channels
+        split(hsvImage, channels);
 
-    // Multiply S channel by scaling factor
-    channels[1] = channels[1] * saturationScale;
+        // Multiply S channel by scaling factor
+        channels[1] = channels[1] * saturationScale;
 
-    // Clipping operation performed to limit pixel values
-    // between 0 and 255
-    min(channels[1], 255, channels[1]);
-    max(channels[1], 0, channels[1]);
+        // Clipping operation performed to limit pixel values
+        // between 0 and 255
+        min(channels[1], 255, channels[1]);
+        max(channels[1], 0, channels[1]);
 
-    // Merge the channels
-    merge(channels, hsvImage);
+        // Merge the channels
+        merge(channels, hsvImage);
 
-    // Convert back from float32
-    hsvImage.convertTo(hsvImage, CV_8UC3);
+        // Convert back from float32
+        hsvImage.convertTo(hsvImage, CV_8UC3);
 
-    Mat imSat;
-    // Convert to BGR color space
-    cv::cvtColor(hsvImage, imSat, COLOR_HSV2BGR);
-
-    imshow("Desaturated Image", imSat);
-    waitKey(0);
-
-    if (argc > 1) {
-        fs::path fs_path = fs::path{img_path};
-        fs::path parentStem = fs_path.parent_path()/fs_path.stem();
+        Mat imSat;
+        // Convert to BGR color space
+        cv::cvtColor(hsvImage, imSat, COLOR_HSV2BGR);
         
-        std:string newPath = std::format(
-            "{}_gray{}", 
-            parentStem.generic_string(),
-            fs_path.extension().generic_string()
-        );
+        if (isImShow == 1) {
+            imshow("Desaturated Image", imSat);
+            waitKey(0);
+        }
 
-        cout << newPath << endl;
-        imwrite(newPath, imSat);
+        if (argc > 1) {
+            std::string newPath = std::format(
+                "{}_gray{}", 
+                parentStem.generic_string(),
+                fs_path.extension().generic_string()
+            );
+
+            cout << newPath << endl;
+            imwrite(newPath, imSat);
+        }
+    }
+    else {
+        cout << "The provided path is not valid!";
     }
 
     return 0;
